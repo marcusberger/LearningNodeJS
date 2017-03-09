8c861eb5b9213b97bbea9659945c58ecbb09bdc3
Marcus Berger <marcusvcberger@gmail.com>
2017-03-09 16:04:44 -0300

    server test

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	res.end("<html><body><h1>Testeeeee</h1></body></html>")
+});
+server.listen(3000);
+
+console.log("Servidor rodando");
+
