d8ce3074036d4fffda14b7c9e0878d156bba5e40
Marcus Berger <marcusvcberger@gmail.com>
2017-03-12 17:13:48 -0300

    modificando server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res){
-	res.end("<html><body><h1>Testeeeee</h1></body></html>")
-});
-server.listen(3000);
+http.createServer(function(request, response){
+	if(request.url == "/produtos"){
+	response.end("<html><body><h1>Listando produtos</h1></body></html>")
+}else{
+	response.end(("<html><body><h1>Home</h1></body></html>"))
+}
+}).listen(3000);
 
 console.log("Servidor rodando");
-
